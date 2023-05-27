d4c2b2e06ecdcc9ee90ad705b857dc5d52a8a3b4
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2023-05-27 08:45:28 -0500

    Update task1.py

--- Class4/task1.py
+++ Class4/task1.py
@@ -1,6 +1,7 @@
-age = int(input("Enter your age: "))
+
 
 while age == age:
+    age = int(input("Enter your age: "))
     if age < 13:
         print("You are not allowed to pass")
     elif age >=13 and age <18:
